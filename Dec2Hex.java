592bda084fe00b63741e959ec532e895ab291ef3
FCowan30 <166056017+FCowan30@users.noreply.github.com>
2024-11-06 21:03:33 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -4,7 +4,16 @@ class Dec2Hex
 {
 public static int Arg1;
     public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
+
+            
+        try {
+            // Try to parse the input argument as an integer
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            // Handle case where input is not a valid integer
+            System.out.println("Error: Invalid input. Please provide a valid integer.");
+            return;
+        }
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
@@ -16,6 +25,10 @@ public static int Arg1;
             rem=num%16;
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
+        }
+        
+        if (hexadecimal.isEmpty()) {
+            hexadecimal = "0";
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
