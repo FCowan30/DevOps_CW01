3effb8b7222cf5388af7e6c0eb4ed111062bd7d8
FCowan30 <166056017+FCowan30@users.noreply.github.com>
2024-11-10 17:18:42 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,50 +1,24 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 class Dec2Hex
 {
-private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
-
-
-    public static void main(String[] args)    {
-
-        final int Arg1;
-
-        // check for input
-        if (args.length == 0){
-            logger.severe("No input Detected");
-            System.exit(1);
-        }
+public static int Arg1;
+    public static void main(String args[])    {
         Arg1 = Integer.parseInt(args[0]);
-        //checks for valid integers as inputs
-        try {
-            if (logger.isLoggable(Level.INFO)){
-                logger.info("Integer Detected");
-             }
-        } catch (NumberFormatException e) {
-           if (logger.isLoggable(Level.SEVERE)) {
-                logger.severe(String.format("The input: '%s' is not a valid integer", args[0]));
-           }
-          System.exit(1);
-        }
-        
-        char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem;
-        int num = Arg1;
-        StringBuilder hexadecimal = new StringBuilder();
-        
-        if (logger.isLoggable(Level.INFO)) {
-            logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
-        }
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
         while(num != 0)
         {
             rem=num%16;
-            hexadecimal.insert(0, ch[rem]);
+            hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-         if (logger.isLoggable(Level.INFO)) {
-             logger.info(String.format("Hexadecimal representation is:  %s", hexadecimal.toString()));
-          }
+
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+
     }
 }
-
